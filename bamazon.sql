2a06ba7f625a0ee2696ad32252718594e13a7e60
Jacqueline Gaherity <jacqueline.gaherity@gmail.com>
2018-11-04 20:03:42 -0800

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,60 +1,26 @@
-Skip to content
- 
-Search or jump to…
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
 
-Pull requests
-Issues
-Marketplace
-Explore
- @Jgaherity Sign out
-1
-0 0 lexluther2125/Bamazon
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
-Bamazon/bamazon.sql
-a639855  on Jul 21, 2017
- Lexi updating products
-     
-30 lines (24 sloc)  1.05 KB
-DROP DATABASE IF EXISTS bamazonDB;
-
-CREATE DATABASE bamazonDB;
-
-USE bamazonDB;
+USE bamazon;
 
 CREATE TABLE products (
-item_id INT NOT NULL AUTO_INCREMENT,
-product_name VARCHAR(300) NULL,
-department_name VARCHAR(300) NULL,
-price DECIMAL(10,2) NULL,
-stock_quantity INT NULL,
-PRIMARY KEY (item_id) 
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(200) NOT NULL,
+  department_name VARCHAR(200) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
 );
 
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES 
-("To Kill A Mockingbird by Harper Lee", "Books", 10.99, 265),
-("E.T. the Extra Terrestrial", "Movies", 12.21, 300),
-("Apple iPad with WiFi, 32GB, Space Gray", "Electronics", 258.99, 6),
-("Modway Engage Upholstered Sofa in Gray", "Home", 923.98, 5),
-("KIND Nuts and Spices Bar, Dark Chocolate/Nuts/Sea Salt 12ct", "Food", 14.05, 375),
-("Neutrogena Makeup Remover Cleansing Towelettes Refill Pack, 25 Count", "Beauty", 18.73, 300),
-("4M Crystal Growing Experiment", "Toys", 16.54, 346),
-("Kate Spade New York Womens Cactus Crosstown", "Clothing", 195, 35),
-("Green earrings glass drop sterling silver", "Handmade", 26.00, 5),
-("Unipack Neoprene Dumbbells", "Sports", 11.93, 54);
-
-
-SELECT * FROM products;
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
+VALUES  ('Wig', 'Beauty', 8.50, 200),
+		('Iphone charger', 'Electronics', 12.25, 1000),
+		('Kerastas Conditioner', 'Beauty', 40.30, 125),
+		('Nike Runners', 'Clothing', 110.00, 78),
+		('Chapstick', 'Cosmetics', 3.20, 670),
+		('Starbucks cup', 'Home', 13.15, 2120),
+		('Kitchen table', 'Home', 350.00, 345),
+		('Mac mouse', 'Electronics', 9.99, 85),
+		('Water bottle', 'Household', 13.50, 2100),
+		('Dove soap', 'beauty', 5.00, 309);
