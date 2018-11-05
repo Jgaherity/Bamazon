72fbe37ebbd931b8d0775fc3f006311d6560dc16
Jacqueline Gaherity <jacqueline.gaherity@gmail.com>
2018-11-04 20:07:11 -0800

    Update customer.js

--- customer.js
+++ customer.js
@@ -1,3 +1,8 @@
+//js file will first display all of the items available for sale. include the IDs, names, & prices for sale 
+//app then prompts:
+// * The first should ask them the ID of the product they would like to buy.
+// * The second message should ask how many units of the product they would like to buy.
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -5,23 +10,22 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: //redacted
-    database: "bamazonDB"
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected as id " + connection.threadId);
     printStock();
 });
 
 
 function printStock() {
     connection.query("SELECT * FROM products", function(err, res) {
+        //console.log(res.length);
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
-        console.log("-----------------------------------\n");
+        console.log("-------------\n");
         runBamazon();
     });
 }
@@ -30,10 +34,10 @@ function runBamazon() {
     inquirer.prompt([{
             type: "input",
             name: "item_id",
-            message: "Please provide the id of the product you wish to purchase.",
+            message: "Please provide the id of the item you wish to purchase.",
             validate: function(input) {
                 if (input === "") {
-                    console.log("You have not entered a valid id number.Please provide the id number of the item you are interested in.");
+                    console.log("Item id invaled. Please provide the id number of the item you are interested in.");
                     return false;
                 } else {
                     return true;
@@ -43,7 +47,7 @@ function runBamazon() {
         {
             type: "input",
             name: "quantity",
-            message: "How many units of this product would you like to purchase?",
+            message: "How many items of this product would you like to purchase? (Please provide integer only)",
             validate: function(input) {
                 if (input === "") {
                     console.log("Please specify how many units you'd like.");
@@ -61,15 +65,15 @@ function runBamazon() {
                 // Update the inventory
                 connection.query("UPDATE products SET stock_quantity = " + (res[0].stock_quantity - input.quantity) + " WHERE item_id = " + input.item_id, function(err, data) {
                     if (err) throw err;
-                    console.log("Thank you for your order! Your total is $" + res[0].price * input.quantity);
-                    console.log("\n---------------------------------------------------------------------\n");
+                    console.log("Thank you for your order,  your total is $" + res[0].price * input.quantity);
+                    console.log("-------------\n");
                     printStock();
                     // End the database connection
                     connection.end();
                 })
             } else {
-                console.log("Sorry, we do not have enough items to fulfill your request.");
-                console.log("\n---------------------------------------------------------------------\n");
+                console.log("Insufficient quantity!");
+                console.log("-------------\n");
             }
         })
     })
